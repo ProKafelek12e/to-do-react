c6ea29f19461255a672dcd77c90e2cbec20eb776
ProKafelek12e <pro.kafelek@gmail.com>
2023-09-22 20:01:57 +0200

    fetch

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import TodoList from './TodoList';
 import { useState } from 'react';
 
+fetch(`http://localhost:3000/gettask`).then(response=> response.json()).then(data=>console.log(data)).catch(error => console.log(error))
+
 function App() {
   const [todos,setTodos] = useState([{id:"1",title:'Tytul1',description:'opis1'},{id:"2",title:'Tytul2',description:'opis2'}])
   return (
