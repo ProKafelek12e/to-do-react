5581f0659c9ae4f1e08490d1fba96e57c5a3e4fd
ProKafelek12e <pro.kafelek@gmail.com>
2023-09-22 15:13:03 +0200

    s

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,7 +3,6 @@ import React from "react";
 export default function TodoList ({todoList}) {
     return (
             todoList.map((task) =>{
-                console.log(task.title)
                 return( 
                 <div key={task.id} className="task">
                     <h1>{task.title}</h1>
