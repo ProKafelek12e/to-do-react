7deefa600aa18289be7493ed644dad184a07c6d0
ProKafelek12e <pro.kafelek@gmail.com>
2023-09-22 22:35:37 +0200

    synced with server

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import TodoList from './TodoList';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
-fetch(`http://localhost:3000/gettask`).then(response=> response.json()).then(data=>console.log(data)).catch(error => console.log(error))
+//fetch(`http://localhost:5000/gettask`).then(response=> response.json())
+//.then(data=>console.log(data)).catch(error => console.log(error))
 
 function App() {
-  const [todos,setTodos] = useState([{id:"1",title:'Tytul1',description:'opis1'},{id:"2",title:'Tytul2',description:'opis2'}])
+  const [todos,setTodos] = useState([{id:"1",title:'Tytul1',description:'opis1'}])
+  useEffect(() => {
+    // Wywołaj fetch, aby pobrać dane z serwera
+    fetch(`http://localhost:5000/gettask`)
+      .then((response) => response.json())
+      .then((data) => {
+        // Aktualizuj stan todos na podstawie danych z serwera
+        setTodos(data);
+      })
+      .catch((error) => console.log(error));
+  })
   return (
     <div className="App">
       <TodoList todoList={todos}></TodoList>
     </div>
   );
+  
 }
 
 export default App;
