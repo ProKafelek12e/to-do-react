1dce572f158ffe90897569cab84b4d4c8c2194a7
ProKafelek12e <pro.kafelek@gmail.com>
2023-09-24 19:45:10 +0200

    style for tasks

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,16 @@
 import React from "react";
+import './todo.css'
 
 export default function TodoList ({todoList}) {
     return (
             todoList.map((task) =>{
                 return( 
                 <div key={task.id} className="task">
-                    <h1>{task.title}</h1>
+                    <div>    
+                        <h1>{task.title}</h1>
+                        <button>✏️</button>
+                        <button>❌</button>
+                    </div>
                     <p>{task.description}</p>
                 
                 </div>
