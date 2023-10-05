354c3a9e61d9f59d965cf0f13d8eae7d5a40e0a4
ProKafelek12e <pro.kafelek@gmail.com>
2023-10-05 09:52:01 +0200

    layout bug

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,11 +6,11 @@ export default function TodoList ({todoList}) {
             todoList.map((task) =>{
                 return( 
                 <div key={task.id} className="task">
-                    <div>    
+                    <span>    
                         <h1>{task.title}</h1>
                         <button>✏️</button>
                         <button>❌</button>
-                    </div>
+                    </span>
                     <p>{task.description}</p>
                 
                 </div>
